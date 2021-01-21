6cf1a2bb614edfc53a9885ffaa8ac517156ce129
Stefan Wuthrich <info@exponity.ch>
2021-01-21 08:07:59 +0100

    primeiro post

--- pages/index.js
+++ pages/index.js
@@ -25,7 +25,7 @@ export default function Home({ posts }) {
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="pt-6 pb-8 space-y-2 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            Latest
+            Ultimos Posts
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
             {siteMetadata.description}
@@ -66,9 +66,9 @@ export default function Home({ posts }) {
                       <Link
                         href={`/blog/${slug}`}
                         className="text-blue-500 hover:text-blue-600 dark:hover:text-blue-400"
-                        aria-label={`Read "${title}"`}
+                        aria-label={`Ler "${title}"`}
                       >
-                        Read more &rarr;
+                        Ler Mais &rarr;
                       </Link>
                     </div>
                   </div>
@@ -85,7 +85,7 @@ export default function Home({ posts }) {
             className="text-blue-500 hover:text-blue-600 dark:hover:text-blue-400"
             aria-label="all posts"
           >
-            All Posts &rarr;
+            Ultimos Posts &rarr;
           </Link>
         </div>
       )}
